0d6d1d4bf5a93872ec0bcb5d127adc39c82c44fc
magas0 <magas0@users.noreply.github.com>
2017-11-07 07:08:59 -0500

    Removed unused package

--- api/resolver.js
+++ api/resolver.js
@@ -1,10 +1,9 @@
 var request = require('request');
-var _ = require('underscore');
 var createTemplate = require('../utils/template.js').resolver;
 
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
-  var term = req.query.text.trim();  
+  var term = req.query.text.trim();
 
   // If the term can be parsed, we are being sent a serialized object from /typeahead
   if (JSON.parse(term)) {
